dd317d516d8cde0fce32a8e01457db6bc369a64a
William Kennedy <bill@ardanstudios.com>
2013-11-07 13:35:43 -0500

    Using Logging Alert

--- helper/catch.go
+++ helper/catch.go
@@ -11,15 +11,12 @@ import (
 // CatchPanic is used to catch any Panic and log exceptions to Stdout. It will also write the stack trace
 func CatchPanic(err *error, goRoutine string, functionName string) {
 	if r := recover(); r != nil {
-
 		// Capture the stack trace
 		buf := make([]byte, 10000)
 		runtime.Stack(buf, false)
 
 		err2 := fmt.Errorf("PANIC Defered [%v] : Stack Trace : %v", r, string(buf))
-
-		tracelog.ERROR(err2, goRoutine, functionName)
-		SendEmail(goRoutine, "Unhandled Exception", err2.Error())
+		tracelog.ALERT("Unhandled Exception", goRoutine, functionName, err2.Error())
 
 		if err != nil {
 			*err = err2
